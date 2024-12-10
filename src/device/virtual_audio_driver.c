9ed88fca785db73f07483e7c2c9c673abb772cc8
AhogeK <ahogek@gmail.com>
2024-12-10 17:22:20 +0800

    feat: VirtualAudioDriver_Initialize

--- src/device/virtual_audio_driver.c
+++ src/device/virtual_audio_driver.c
@@ -5,10 +5,47 @@
 #include "device/virtual_audio_driver.h"
 #include <stdatomic.h>
 #include <pthread.h>
+#include "mach/mach_time.h"
 
-// 全局变量定义
+// 定义插件状态相关的全局变量
 static pthread_mutex_t gPlugIn_StateMutex = PTHREAD_MUTEX_INITIALIZER;
-static UInt32 gPlugIn_RefCount = 1;
+static UInt32 gPlugIn_RefCount = 0;
+static AudioServerPlugInHostRef gPlugIn_Host = NULL;
+static const AudioObjectPropertySelector kPlugIn_CustomPropertyID = 0x50437374; // 'PCst'
+
+// 定义盒子相关的全局变量
+static CFStringRef gBox_Name = NULL;
+static Boolean gBox_Acquired = true;
+
+// 定义设备相关的全局变量
+static pthread_mutex_t gDevice_IOMutex = PTHREAD_MUTEX_INITIALIZER;
+static Float64 gDevice_SampleRate = 44100.0;
+static UInt64 gDevice_IOIsRunning = 0;
+static const UInt32 kDevice_RingBufferSize = 16384;
+static Float64 gDevice_HostTicksPerFrame = 0.0;
+static UInt64 gDevice_NumberTimeStamps = 0;
+static Float64 gDevice_AnchorSampleTime = 0.0;
+static UInt64 gDevice_AnchorHostTime = 0;
+
+// 定义流相关的全局变量
+static bool gStream_Input_IsActive = true;
+static bool gStream_Output_IsActive = true;
+
+// 定义音量相关的全局变量
+static const Float32 kVolume_MinDB = -96.0f;
+static const Float32 kVolume_MaxDB = 6.0f;
+static Float32 gVolume_Input_Master_Value = 0.0f;
+static Float32 gVolume_Output_Master_Value = 0.0f;
+
+// 定义静音相关的全局变量
+static bool gMute_Input_Master_Value = false;
+static bool gMute_Output_Master_Value = false;
+
+// 定义数据源相关的全局变量
+static const UInt32 kDataSource_NumberItems = 4;
+static UInt32 gDataSource_Input_Master_Value = 0;
+static UInt32 gDataSource_Output_Master_Value = 0;
+static UInt32 gDataDestination_PlayThru_Master_Value = 0;
 
 #pragma mark The Interface
 
@@ -154,6 +191,65 @@ static ULONG VirtualAudioDriver_Release(void *inDriver) {
     }
     theAnswer = gPlugIn_RefCount;
     pthread_mutex_unlock(&gPlugIn_StateMutex);
+
+    Done:
+    return theAnswer;
+}
+
+#pragma mark Basic Operations
+
+static OSStatus VirtualAudioDriver_Initialize(AudioServerPlugInDriverRef inDriver, AudioServerPlugInHostRef inHost) {
+    // 该方法的作用是初始化驱动。一个特定的任务是存储 AudioServerPlugInHostRef 以便后续使用。
+    // 注意，当此调用返回时，HAL 将扫描驱动维护的各个列表（如设备列表）以获取驱动正在发布的初始对象集。
+    // 因此，无需通知 HAL 关于在执行此方法过程中创建的任何对象。
+
+    // 声明局部变量
+    OSStatus theAnswer = 0;
+
+    // 检查参数
+    if (inDriver != gAudioServerPlugInDriverRef) {
+        theAnswer = kAudioHardwareBadObjectError;
+        goto Done;
+    }
+
+    // 存储 AudioServerPlugInHostRef
+    gPlugIn_Host = inHost;
+
+    // 从设置中初始化 box acquired 属性
+    CFPropertyListRef theSettingsData = NULL;
+    gPlugIn_Host->CopyFromStorage(gPlugIn_Host, CFSTR("box acquired"), &theSettingsData);
+    if (theSettingsData != NULL) {
+        if (CFGetTypeID(theSettingsData) == CFBooleanGetTypeID()) {
+            gBox_Acquired = CFBooleanGetValue((CFBooleanRef) theSettingsData);
+        } else if (CFGetTypeID(theSettingsData) == CFNumberGetTypeID()) {
+            SInt32 theValue = 0;
+            CFNumberGetValue((CFNumberRef) theSettingsData, kCFNumberSInt32Type, &theValue);
+            gBox_Acquired = theValue ? 1 : 0;
+        }
+        CFRelease(theSettingsData);
+    }
+
+    // 从设置中初始化 box name
+    gPlugIn_Host->CopyFromStorage(gPlugIn_Host, CFSTR("box name"), &theSettingsData);
+    if (theSettingsData != NULL) {
+        if (CFGetTypeID(theSettingsData) == CFStringGetTypeID()) {
+            gBox_Name = (CFStringRef) theSettingsData;
+            CFRetain(gBox_Name);
+        }
+        CFRelease(theSettingsData);
+    }
+
+    // 如果没有设置 box name，则直接设置一个默认值
+    if (gBox_Name == NULL) {
+        gBox_Name = CFSTR("Virtual Audio Box");
+    }
+
+    // 计算每帧的主机时钟周期
+    struct mach_timebase_info theTimeBaseInfo;
+    mach_timebase_info(&theTimeBaseInfo);
+    Float64 theHostClockFrequency = (Float64) theTimeBaseInfo.denom / (Float64) theTimeBaseInfo.numer;
+    theHostClockFrequency *= 1000000000.0;
+    gDevice_HostTicksPerFrame = theHostClockFrequency / gDevice_SampleRate;
 
     Done:
     return theAnswer;
